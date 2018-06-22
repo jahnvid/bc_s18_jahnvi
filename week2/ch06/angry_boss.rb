760e9e3f6d62ff470185089a62caa0149f28fafd
jahnvid <jahnvidoshi@berkeley.edu>
2018-06-22 06:32:23 -0700

    complete

---
+++ week2/ch06/angry_boss.rb
@@ -0,0 +1,3 @@
+puts "What would you like to say to the boss?"
+req = gets.chomp.upcase
+puts "WHAT DO YOU MEAN '" + req + "'???!! YOU'RE FIRED!!!"
---
+++ week2/ch06/table_of_contents.rb
@@ -0,0 +1,5 @@
+line_width = 50
+puts( ("Table of Contents").center(line_width))
+puts(("Getting Started").ljust(line_width/2) + ("1").rjust(line_width/2))
+puts(("Numbers").ljust(line_width/2) + ("9").rjust(line_width/2))
+puts(("Letters").ljust(line_width/2) + ("13").rjust(line_width/2))

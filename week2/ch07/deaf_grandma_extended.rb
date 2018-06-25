84a367d33aa6c78dfe9fb7869e589bd12bc09766
jahnvid <jahnvidoshi@berkeley.edu>
2018-06-24 23:28:32 -0700

    complete

--- week2/ch07/deaf_grandma_extended.rb
+++ week2/ch07/deaf_grandma_extended.rb
@@ -1,24 +1,18 @@
 puts"What do you wanna say to grandma"
 ans = gets.chomp
 c = 0
-while true
+while c <= 1
   if ans != ans.upcase
   puts("WHAT!! SPEAK UP HONEY!!")
+  c = 0
   ans = gets.chomp
-  else
-    if ans == "BYE"
-      puts"WHAT"
-      ans = gets.chomp
-    end
-      if ans == "BYE"
-        puts "WHAT"
+  elsif ans == "BYE"
+        c = c + 1
+        puts("WHAT")
         ans = gets.chomp
-      end
-        if ans == "BYE"
-          puts"GOODBYE"
-          break
-end
+  else
     puts("NO! NOT SINCE " + (1930 + rand(21)).to_s + "!!")
+    c = 0
     ans = gets.chomp
   end
 end
